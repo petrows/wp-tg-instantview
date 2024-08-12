6c81cac7c908f0e394f00581fc3e63b3c78ec309
Petro <petro@petro.ws>
2024-08-12 18:57:56 +0200

    Version 1.4: plugin fixes, mabe by user suggestions:
    * New option to ON/OFF displayed post date
    * New option to ON/OFF displayed post author name
    * Plugin will work without OpenGraph plugins installed (provide proper meta to TG bot) and reuse existing, if your blog sets custom post share metadata

--- tg-instantview/tg-display.php
+++ tg-instantview/tg-display.php
@@ -7,17 +7,63 @@ Based on: https://gist.github.com/fishchev/ed2ca15d5ffd9594d41498a4bf9ba12e
 if (!defined("ABSPATH")) {
     exit;
 }
-$tg_options = get_option( 'tgiv_instantview_render' );
+// Get Plugin settings array
+$tgiv_options = tgiv_options();
+// Check for default values
+
+// Get default WP meta tags, going to be displayed for this post
+$tgiv_wp_meta = tgiv_extract_meta();
+
+// Prepare list of meta tags, rendered for preview
+$tgiv_meta = array();
+// Required to render:
+$tgiv_meta['tg:site_verification'] = 'g7j8/rPFXfhyrq5q0QQV7EsYWv4=';
+// Channel name?
+if ($tgiv_options['tgiv_channel_name']) {
+	$tgiv_meta['telegram:channel'] = $tgiv_options['tgiv_channel_name'];
+}
+
+// Published date: article:published_time
+if ($tgiv_options['tgiv_display_date']) {
+	$tgiv_meta['article:published_time'] = get_the_date('c');
+} else {
+	// Date disabled, display none
+	$tgiv_meta['article:published_time'] = '';
+}
+// Author: article:author
+if ($tgiv_options['tgiv_display_author']) {
+	$tgiv_meta['article:author'] = get_the_author();
+} else {
+	// Author disabled, display none
+	$tgiv_meta['article:author'] = '';
+}
+// Image: og:image
+if (isset($tgiv_wp_meta['og:image'])) {
+	$tgiv_meta['og:image'] = $tgiv_wp_meta['og:image'];
+} else {
+	// If not set, get from WP
+	$tgiv_meta['og:image'] = get_the_post_thumbnail_url();
+}
+// Site name: og:site_name
+$tgiv_meta['og:site_name'] = get_bloginfo('name');
+// Title: og:title
+$tgiv_meta['og:title'] = get_the_title();
+// Short text: og:description
+if (isset($tgiv_wp_meta['og:description'])) {
+	$tgiv_meta['og:description'] = $tgiv_wp_meta['og:description'];
+} else {
+	// If not set, fill with excert
+	$tgiv_meta['og:description'] = get_the_excerpt();
+}
+
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-	<?php if (isset($tg_options['tgiv_channel_name']) && strlen($tg_options['tgiv_channel_name'])) { ?>
-    <meta property="telegram:channel" content="<?php echo esc_html($tg_options['tgiv_channel_name']); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>" />
+	<?php foreach ($tgiv_meta as $tgiv_meta_key => $tgiv_meta_value) { ?>
+    <meta property="<?php echo esc_html($tgiv_meta_key); ?>" content="<?php echo esc_html($tgiv_meta_value); ?>" />
 	<?php } ?>
-    <meta property="tg:site_verification" content="g7j8/rPFXfhyrq5q0QQV7EsYWv4=">
-    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
